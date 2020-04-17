c9cea4e4c03fffef62dbf84ee3018be20ca349c5
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2020-04-17 20:50:44 +0100

    Implementations.

--- familyapp/lib/theme.dart
+++ familyapp/lib/theme.dart
@@ -4,6 +4,13 @@ Color primaryColor = Color(0xFFFFFFFC);
 Color primaryText = Color(0xFF757575);
 Color buttonHeader = Color(0xFFEDEDFA);
 
+final List<Color> categoriesColor = [
+  Color(0xFFFA382C),
+  Color(0xFF6158EC),
+  Color(0xFF34BFCB),
+  Color(0xFF4B547E),
+];
+
 TextStyle headlines = TextStyle(fontSize: 20, color: primaryText, fontFamily: 'PTsans', fontWeight: FontWeight.w600);
 TextStyle headCategories = TextStyle(fontSize: 15, color: primaryText, fontFamily: 'PTsans', fontWeight: FontWeight.w500);
 
--- familyapp/lib/ui/landingpage.dart
+++ familyapp/lib/ui/landingpage.dart
@@ -9,20 +9,20 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   int selectedindex = 0;
   int pageIndex = 1;
+  int bottomIndex = 0;
 
   PageController _pageController;
+  PageController _pageController2;
 
-  final List<String> categories = [
-    'Freebies',
-    'Offers',
-    'Contests',
-    'Others'
-  ];
+  Color buttonColor = categoriesColor[0];
+
+  final List<String> categories = ['Freebies', 'Offers', 'Contests', 'Others'];
 
   @override
   void initState() {
     super.initState();
     _pageController = PageController(initialPage: 1, viewportFraction: 0.6);
+    _pageController2 = PageController(initialPage: 1, viewportFraction: 0.88);
   }
 
   @override
@@ -49,6 +49,28 @@ class _LandingPageState extends State<LandingPage> {
             ),
           ],
         ),
+        bottomNavigationBar: BottomNavigationBar(
+//          elevation: 2,
+            selectedItemColor: primaryText,
+            unselectedItemColor: Color(0xFFE2E2E2),
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            onTap: (int index) {
+              setState(() {
+                bottomIndex = index;
+              });
+            },
+            currentIndex: bottomIndex,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(CustomIcons.home), title: Text('')),
+              BottomNavigationBarItem(icon: Icon(Icons.star), title: Text('')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.favorite), title: Text('')),
+              BottomNavigationBarItem(icon: Icon(Icons.info), title: Text('')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person), title: Text('')),
+            ]),
         backgroundColor: primaryColor,
         body: SingleChildScrollView(
           child: Column(
@@ -59,7 +81,14 @@ class _LandingPageState extends State<LandingPage> {
                 child: ListView.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: categories.length,
+//                    controller: ,
                     itemBuilder: (BuildContext context, int index) {
+                      double value = 1;
+                      if (_pageController.position.haveDimensions) {
+                        value = _pageController.page - index;
+                        value =
+                            (1 - (value.abs() * 0.62) + 0.09).clamp(0.0, 1.0);
+                      }
                       return Padding(
                         padding: const EdgeInsets.all(2.0),
                         child: SizedBox(
@@ -70,16 +99,17 @@ class _LandingPageState extends State<LandingPage> {
                                     fontSize: 13.5,
                                     color: selectedindex == index
                                         ? Colors.white
-                                        : primaryText,
+                                        : categoriesColor[index],
                                     fontFamily: 'PTsans',
                                     fontWeight: FontWeight.w600)),
                             onPressed: () {
                               setState(() {
                                 selectedindex = index;
+                                buttonColor = categoriesColor[index];
                               });
                             },
                             color: selectedindex == index
-                                ? Colors.red
+                                ? buttonColor
                                 : buttonHeader,
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(10)),
@@ -87,9 +117,6 @@ class _LandingPageState extends State<LandingPage> {
                         ),
                       );
                     }),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
               ),
               Container(
                 height: 250,
@@ -108,6 +135,64 @@ class _LandingPageState extends State<LandingPage> {
                   },
                 ),
               ),
+
+              Padding(
+                padding: EdgeInsets.all(5),
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: Text(
+                      'Featured',
+                      style: headlines,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: SizedBox(
+                      width: 83,
+                      child: FlatButton(
+                        child: Text('Show all',
+                            style: TextStyle(
+                                fontSize: 13.5,
+                                color: buttonColor,
+                                fontFamily: 'PTsans',
+                                fontWeight: FontWeight.w600)),
+                        onPressed: () {
+                          setState(() {});
+                        },
+                        color: Color(0xFFFAF5F3),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              Padding(
+                padding: EdgeInsets.all(1),
+              ),
+
+              Container(
+                height: 250,
+                width: double.infinity,
+                child: PageView.builder(
+                  controller: _pageController2,
+                  itemCount: 3,
+//                  pageSnapping: false,
+                  onPageChanged: (int index) {
+
+                  },
+                  itemBuilder: (BuildContext context, int index) {
+                    return _familySelector(index);
+                  },
+                ),
+              ),
+              //TODO: implement featured list
             ],
           ),
         ));
@@ -150,47 +235,90 @@ class _LandingPageState extends State<LandingPage> {
               ),
             ),
           )),
-
-          pageIndex == index ?
-          Positioned(
-            right: 1,
-            bottom: 11,
-            child: FloatingActionButton(
-              onPressed: (){} ,
+          pageIndex == index
+              ? Positioned(
+                  right: 1,
+                  bottom: 11,
+                  child: FloatingActionButton(
+                    onPressed: () {},
 //              elevation: 2.7,
-              child: Icon(Icons.add),
-              backgroundColor: Colors.red,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-            ),
-          ) : Container(),
-
-          pageIndex == index ?
-          Positioned(
-            bottom: 1,
-            width: 200,
-            child: Container(
-              child: Center(
-                child: Column(
-                  children: <Widget>[
-                    Text('Special Offer',
-                        style: TextStyle(
-                            fontSize: 14.5,
-                            color: Color(0xFFD7D5D7),
-                            fontFamily: 'PTsans',
-                            fontWeight: FontWeight.w600)),
-//                    Text('Share & Gain',
-//                        style: TextStyle(
-//                            fontSize: 16,
-//                            color: Color(0xFF757575),
-//                            fontFamily: 'PTsans',
-//                            fontWeight: FontWeight.w600)),
-                  ],
-                ),
-              ),
-            ),
-          ): Container()
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      size: 23,
+                    ),
+                    backgroundColor: buttonColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                )
+              : Container(),
+          pageIndex == index
+              ? Positioned(
+                  bottom: 1,
+                  width: 200,
+                  child: Container(
+                    child: Center(
+                      child: Column(
+                        children: <Widget>[
+                          Text('Special Offer',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Color(0xFFD7D5D7),
+                                  fontFamily: 'PTsans',
+                                  fontWeight: FontWeight.w600)),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              : Container()
         ],
       ),
     );
   }
+
+  _familySelector(int index) {
+    return AnimatedBuilder(
+      animation: _pageController2,
+      builder: (BuildContext context, Widget widget) {
+        double value = 1;
+        if (_pageController2.position.haveDimensions) {
+          value = _pageController2.page - index;
+          value = (1 - (value.abs() * 0.2) + 0.01).clamp(0.0, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 270,
+            width: Curves.easeInOut.transform(value) * 300,
+            child: widget,
+          ),
+        );
+      },
+      child: Stack(
+        children: <Widget>[
+          Center(
+//              widthFactor: 2,
+              child: Container(
+                margin: EdgeInsets.only(right: 50),
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                      color: Color(0xFFE2E0E2),
+                      blurRadius: 10.0,
+                      offset: Offset(4.0, 3.0))
+                ]),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/images/family.jpg',
+                    width: 230,
+                    height: 200,
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+
 }
